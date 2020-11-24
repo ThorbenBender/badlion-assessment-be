9c98981e3e7baa173f725fce740207f027e10025
Thorben <bender.thorben0@gmail.com>
2020-11-24 14:45:45 +0100

    Add axios

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 let app = require('express')();
-let request = require('request');
+let axios = require('axios');
 
 app.get('/', (req, res) => {
   res.send('Hello');
 });
 
-app.get('/league/:id', async (req, res) => {
-  res.send('Hello');
+app.get('/leagues/:id', async (req, res) => {
+  try {
+    let data = await request(
+      `https://api.eslgaming.com/play/v1/leagues/${req.params.id}`,
+      {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      }
+    );
+    res.json(data);
+  } catch (err) {}
 });
 
 app.listen(5000, () => {
