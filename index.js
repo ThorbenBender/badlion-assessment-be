5363784e05aff0e3920651d1b4d62c19c0b7b582
Thorben <bender.thorben0@gmail.com>
2020-11-24 15:26:26 +0100

    add route for results

--- index.js
+++ index.js
@@ -5,9 +5,26 @@ app.get('/', (req, res) => {
   res.send('Hello');
 });
 
-app.get('/leagues/:id', async (req, res) => {
+app.get('/leagues/:id', (req, res) => {
   request(
     `https://api.eslgaming.com/play/v1/leagues/${req.params.id}`,
+    {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    (err, data, body) => {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(body);
+      }
+    }
+  );
+});
+
+app.get('/leagues/:id/results', (req, res) => {
+  request(
+    `https://api.eslgaming.com/play/v1/leagues/${req.params.id}/results`,
     {
       'Access-Control-Allow-Origin': '*',
       'Content-Type': 'application/x-www-form-urlencoded',
